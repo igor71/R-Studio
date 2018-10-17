2d0e60f49a053b55ed4657dd1de1de528757c5f3
igor71 <34439207+igor71@users.noreply.github.com>
2018-10-17 14:31:03 +0300

    Update and rename R-Build-All to R-Build-All.sh

---
+++ R-Build-All.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+EXTRA_FLAGS="--no-cache"
+
+R_VER=0.0
+RSTUDIO_VER=0.0
+
+docker build $EXTRA_FLAGS -f Dockerfile.r-ver -t yi/r-base :${R_VER} . && \
+docker build $EXTRA_FLAGS -f Dockerfile.r-studio -t yi/tflow-gui:${RSTUDIO_VER} . 
